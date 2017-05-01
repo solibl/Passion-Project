4367f31dfa92f836b7700b46f074ef8d055082f9
Brian Lee <brianleedongjun@gmail.com>
2017-04-30 17:27:18 -0700

    Add base layout to page

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+openNav()
+closeNav()
 });
+
+    function openNav() {
+      document.getElementById("mySidenav").style.width = "250px";
+      document.getElementById("main").style.marginLeft = "250px";
+    }
+
+    function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+      document.getElementById("main").style.marginLeft= "0";
+    }
