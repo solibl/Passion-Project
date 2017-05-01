901f77b0c35901ba67afea72cdb07f6e77c30c44
Brian Lee <brianleedongjun@gmail.com>
2017-04-30 18:48:29 -0700

    Add login feature to create a session

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -3,15 +3,24 @@ get '/users/new' do
 end
 
 get '/users/login' do
-	
+	erb :'users/login'
+end
+
+get '/users/:id' do
 end
 
 post '/users' do
 	@user = User.new(params[:user])
 	if @user.save
-		redirect "/user/#{@user.id}"
+		redirect "/users/login"
 	else
 		@errors = @user.errors.full_messages
 		erb :'/users/new'
 	end
+end
+
+post '/users/login' do
+	@user = User.find_by(username: params["username"])
+	login(@user)
+	redirect "/users/#{@user.id}"
 end
