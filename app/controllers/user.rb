db9ef053fb43729204d6ae2dffbf4a3773b3e29d
Brian Lee <brianleedongjun@gmail.com>
2017-04-30 20:41:46 -0700

    Add filler users for seed file

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -44,6 +44,11 @@ end
 
 post '/users/login' do
 	@user = User.find_by(username: params["username"])
+	if @user != nil
 	login(@user)
 	redirect "/users/#{@user.id}"
+	else
+		@errors = ["Invalid Username or Password"]
+		erb :'/users/login'
+	end
 end
