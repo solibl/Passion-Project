4e499fdb9ca8dd1d4999ee9e76d4223dc9003d36
Brian Lee <brianleedongjun@gmail.com>
2017-04-30 22:06:48 -0700

    Add current game playing feature to user page

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -14,6 +14,13 @@ end
 get '/users/:id' do
 	@user = User.find(params[:id])
 	@current_user = User.find(session[:id])
+	if @current_user.game_id == nil
+		@current_game = Game.find(1)
+		@game_selection = build_game_selection(@current_game.game_name)
+	else
+		@current_game = Game.find(@current_user.game_id)
+		@game_selection = build_game_selection(@current_game.game_name)
+	end
 	erb :'/users/show'
 end
 
@@ -41,10 +48,18 @@ end
 post '/users/login' do
 	@user = User.find_by(username: params["username"])
 	if @user != nil
-	login(@user)
-	redirect "/users/#{@user.id}"
+		login(@user)
+		redirect "/users/#{@user.id}"
 	else
 		@errors = ["Invalid Username or Password"]
 		erb :'/users/login'
 	end
+end
+
+patch '/users/game' do
+	@current_game_playing = Game.find_by(game_name: params["game"])
+	@current_user = User.find(session[:id])
+	@current_user.game_id = @current_game_playing.id
+	@current_user.save
+	redirect "/users/#{@current_user.id}"
 end
